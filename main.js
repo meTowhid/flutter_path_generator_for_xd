8952f82d45dccce64ac91a0257a3136e45848c09
towhid <bt.towhid@gmail.com>
2020-07-23 00:05:28 +0600

    code generator logic updated, artboard size and shift in artboard feature added

--- main.js
+++ main.js
@@ -1,50 +1,89 @@
-
 const clipboard = require("clipboard");
 const { selection } = require("scenegraph")
 let panel;
 let flutterCodes = "";
+let pathShapes = [];
 
 function create() {
-    const HTML =
-        `<style>
-            .break {
-                flex-wrap: wrap;
-            }
-            label.row > span {
-                color: #8E8E8E;
-                width: 20px;
-                text-align: right;
-                font-size: 7px;
-            }
-            label.row input {
-                flex: 1 1 auto;
-            }
-            .show {
-                display: block;
-            }
-            .hide {
-                display: none;
-            }
-        </style>
-        <form method="dialog" id="main">
-            <div class="row break">
-                <p id="flutter_code" uxp-quiet="true"/>
-            </div>
-            <footer><button id="ok" type="submit" uxp-variant="cta">Copy</button></footer>
-        </form>
-        <p id="warning">This plugin requires you to select path in the document. Please select a path.</p>
-        `
-    function copyCode() {
-        clipboard.copyText(flutterCodes);
-        console.log("Code generated and copied to clipboard");
-        console.log(flutterCodes);
+    const HTML = `
+    <style>
+    .break {
+        flex-wrap: wrap;
     }
+
+    label.row>span {
+        color: #8E8E8E;
+        width: 5px;
+        text-align: right;
+        font-size: 12px;
+    }
+
+    label.row input {
+        flex: 1 1 auto;
+    }
+
+    .show {
+        display: block;
+    }
+
+    .hide {
+        display: none;
+    }
+    </style>
+    <form method="dialog" id="main">
+        <h5>Area</h5>
+        <div class="row break">
+            <label class="row">
+                <span>↔︎</span>
+                <input type="number" uxp-quiet="true" id="txtW" value="10" size=10 placeholder="Width" />
+            </label>
+            <label class="row">
+                <span>↕︎</span>
+                <input type="number" uxp-quiet="true" id="txtH" value="10" size=10 placeholder="Height" />
+            </label>
+        </div>
+        <label>
+            <input type="checkbox" id="cb_artboard" checked>Match artboard size<br>
+        </label>
+        <br><br>
+        <h5>Flutter Code</h5>
+        <div class="row break">
+            <p id="flutter_code" uxp-quiet="true" style="font-size:140%;"/>
+        </div>
+        <footer><button id="ok" type="submit" uxp-variant="cta">Copy</button></footer>
+    </form>
+    <p id="warning">Select a path please.</p>
+        `
 
     panel = document.createElement("div");
     panel.innerHTML = HTML;
-    panel.querySelector("form").addEventListener("submit", copyCode);
+    panel.querySelector("form").addEventListener("submit", function () {
+        clipboard.copyText(flutterCodes);
+        console.log("Code generated and copied to clipboard");
+        console.log(flutterCodes);
+    });
+    panel.querySelector("#cb_artboard").addEventListener("change", updateAreaInfo);
 
     return panel;
+}
+
+function updateAreaInfo() {
+    let width = panel.querySelector("#txtW");
+    let height = panel.querySelector("#txtH");
+    let isChecked = panel.querySelector("#cb_artboard").checked;
+
+    width.readOnly = isChecked;
+    height.readOnly = isChecked;
+
+    let area = isChecked ? selection.focusedArtboard.localBounds : selection.items[0].localBounds;
+
+    console.log(area);
+
+    width.value = round(area.width);
+    height.value = round(area.height);
+
+    generatePathData(pathShapes);
+    document.querySelector("#flutter_code").innerHTML = flutterCodes.trim().replace(/\n/g, "<br>");
 }
 
 function show(event) {
@@ -62,7 +101,7 @@ function update() {
         console.log("No object selected!");
     }
 
-    let pathShapes = [];
+    pathShapes = [];
     selection.items.forEach(element => {
         if (element.constructor.name == "Path") pathShapes.push(element);
     });
@@ -74,20 +113,29 @@ function update() {
     } else {
         form.className = "show";
         warning.className = "hide";
-        generatePathData(pathShapes);
-        document.querySelector("#flutter_code").innerHTML = flutterCodes;
+        updateAreaInfo(pathShapes);
     }
 }
 
 function generatePathData(pathShapes) {
     flutterCodes = "";
+    let inputWidth = panel.querySelector("#txtW").value;
+    let inputHeight = panel.querySelector("#txtH").value;
+    let isChecked = panel.querySelector("#cb_artboard").checked;
 
     pathShapes.forEach(path => {
         let segments = extractedPathData(path.pathData);
         let offset = findOffsetPoint(segments);
 
         let normalized = normalizePoints(segments, offset);
-        let code = generateCode(normalized, path.localBounds.width, path.localBounds.height);
+        let code = generateCode(normalized, {
+            "reposition": isChecked,
+            "x": path.topLeftInParent.x,
+            "y": path.topLeftInParent.y,
+            "name": path.name.toLowerCase().replace(/ /g, "_"),
+            "width": isChecked ? inputWidth : round(path.localBounds.width),
+            "height": isChecked ? inputHeight : round(path.localBounds.height)
+        });
 
         console.log(code);
         flutterCodes += (code + "\n\n");
@@ -147,13 +195,14 @@ const normalizePoints = (pathData, offsetPoint) => {
 
 const round = (number) => { return Math.round(number * 100) / 100; }
 
-function generateCode(path, shapeWidth, shapeHeight) {
+function generateCode(path, props) {
+    let xs = props.name + "_xs";
+    let ys = props.name + "_ys";
     var code = [
-        ["double shapeWidth = " + round(shapeWidth) + ";", 1],
-        ["double shapeHeight = " + round(shapeHeight) + ";", 1],
-        ["double xs = size.width / shapeWidth;", 1],
-        ["double ys = size.height / shapeHeight;", 1],
-        ["Path path = Path()", 2]
+        ["    // " + props.name, 0],
+        ["double " + xs + " = size.width / " + round(props.width) + ";", 1],
+        ["double " + ys + " = size.height / " + round(props.height) + ";", 1],
+        ["Path " + props.name + " = Path()", 2]
     ];
 
     for (var segmentId in path) {
@@ -162,23 +211,23 @@ function generateCode(path, shapeWidth, shapeHeight) {
             case "M":
                 code.push([
                     "    ..moveTo("
-                    + segment[1] + " * xs, " + segment[2] + " * ys"
+                    + segment[1] + " * " + xs + ", " + segment[2] + " * " + ys + ""
                     + ")"
                     , 1]);
                 break;
             case "L":
                 code.push([
                     "    ..lineTo("
-                    + segment[1] + " * xs, " + segment[2] + " * ys"
+                    + segment[1] + " * " + xs + ", " + segment[2] + " * " + ys + ""
                     + ")"
                     , 1]);
                 break;
             case "C":
                 code.push([
                     "    ..cubicTo("
-                    + segment[1] + " * xs, " + segment[2] + " * ys,"
-                    + segment[3] + " * xs, " + segment[4] + " * ys,"
-                    + segment[5] + " * xs, " + segment[6] + " * ys"
+                    + segment[1] + " * " + xs + ", " + segment[2] + " * " + ys + ","
+                    + segment[3] + " * " + xs + ", " + segment[4] + " * " + ys + ","
+                    + segment[5] + " * " + xs + ", " + segment[6] + " * " + ys + ""
                     + ")"
                     , 1]);
                 break;
@@ -190,16 +239,22 @@ function generateCode(path, shapeWidth, shapeHeight) {
         }
     }
 
+    code[code.length - 1][0] += ";";
+
+    let ox = round(props.x);
+    let oy = round(props.y);
+    if (props.reposition && (ox != 0.0 || oy != 0.0)) code.push([
+        props.name + " = " + props.name + ".shift(Offset(" + ox + " * " + xs + ", " + oy + " * " + ys + "));"
+        , 2]);
+
     var returnCode = "";
     for (var lineId in code) {
         var line = code[lineId];
-
         for (var i = 0; i < line[1]; i++) returnCode += "\n";
-
         returnCode += line[0];
     }
 
-    return returnCode + ";";
+    return returnCode;
 }
 
 module.exports = {
